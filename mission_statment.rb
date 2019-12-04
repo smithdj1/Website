6fc20004d110bbba1d28b85c22c35cf9a1c0675b
David Smith <davids3987@gmail.com>
2019-12-04 10:59:38 -0700

    Initial setup of page

---
+++ mission_statment.rb
@@ -0,0 +1,5 @@
+Target Audience         -> Skiers
+Problem I Will Solve    -> Find the distance and keep track of what trails
+have been skied on
+Why It Will Be Useful   ->Help track the progress of skiiers throughtout the 
+season
---
+++ website.js
@@ -0,0 +1,6 @@
+function ready() {
+    setInterval (() =>{
+        document.body.style.background = "Teal"
+    })
+}
+document.addEventListener("DOMContentLoaded", ready);
