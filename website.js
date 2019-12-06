6091cee0b6c613c9729d778cd0b1a6fe497dba1a
David Smith <davids3987@gmail.com>
2019-12-06 10:23:31 -0700

    tried to define trail sizes

--- website.js
+++ website.js
@@ -2,5 +2,15 @@ function ready() {
     setInterval (() =>{
         document.body.style.background = "Teal"
     })
+    let x = 0
+    black_bear.onclick = function() {
+        console.log("page ready")
+        x = x + 1
+    }
+    long_chutes.onclick = function() {
+        console.log("page ready")
+        x = x + 2
+    }
+    final_num.innerHTML = x;
 }
 document.addEventListener("DOMContentLoaded", ready);
